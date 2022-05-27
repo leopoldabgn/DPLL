69c601402dd0e196b2c7418182543c032f6d8237
Leopold Abignoli <leopold.abignoli@gmail.com>
2022-05-27 14:39:31 +0200

    Ajout fonction check_monotony pour la regle de (3) de DPLL

--- dpll.c
+++ dpll.c
@@ -4,6 +4,8 @@
 #include "cnf.h"
 
 static void help();
+int check_monotony(CNF* cnf, litteral l);
+static int check_monotony_clause(clause c, litteral l);
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
@@ -14,6 +16,8 @@ int main(int argc, char* argv[]) {
     CNF* cnf = string_to_CNF(argv[1]);
 
     print_CNF(cnf);
+    litteral l = {.name='x', .isnot=0};
+    printf("monotonie %c : %d\n", l.name, check_monotony(cnf, l));
 
     free_CNF(cnf);
     return EXIT_SUCCESS;
@@ -25,4 +29,33 @@ static void help() {
     puts("\":\" represente le NOT().");
     puts("Vous pouvez le remplacer par n'importe");
     puts("quel caractere non-alphabetic.");
+}
+
+int check_monotony(CNF* cnf, litteral l) {
+    if(cnf == NULL)
+        return 0;
+    int not_in_cnf = 1, monotony = 0;
+    for(int i=0;i<cnf->size;i++) {
+        monotony = check_monotony_clause(cnf->clauses[i], l);
+        if(!monotony)
+            return 0;
+        not_in_cnf = monotony == 2 ? not_in_cnf : 0;
+    }
+    return not_in_cnf ? 2 : 1;
+}
+
+// 0-> pas monotone, 1-> monotone, 2-> absent.
+static int check_monotony_clause(clause c, litteral l) {
+    if(c.litts == NULL)
+        return 0;
+    int not_in_clause = 1;
+    for(int i=0;i<c.size;i++) {
+        if(l.name == c.litts[i].name) {
+            if(l.isnot == c.litts[i].isnot)
+                not_in_clause = 0;
+            else
+                return 0;
+        }
+    }
+    return not_in_clause ? 2 : 1;
 }
